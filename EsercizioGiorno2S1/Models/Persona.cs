f5d3b7884af145ac5fc84ef302883eef0d85510d
Francesca-palmeri <francesca.palmeri.c@gmail.com>
2025-02-11 16:00:56 +0100

    Aggiungere i file di progetto.

---
+++ EsercizioGiorno2S1/Models/Persona.cs
@@ -0,0 +1,36 @@
+﻿
+
+namespace EsercizioGiorno2S1.Models
+{
+    
+        public class Persona
+        {
+            public string Nome { get; set; } = "Francesca";
+            public string Cognome { get; set; } = "Palmeri";
+            public int Eta { get; set; } = 30;
+
+
+
+            public void GetNome(string Nome)
+            {
+                Console.WriteLine("Nome: " + Nome);
+            }
+
+            public void GetCognome(string Cognome)
+            {
+                Console.WriteLine("Cognome:" + Cognome);
+            }
+
+            public void GetEta(int Eta)
+            {
+                Console.WriteLine("Età: " + Eta);
+            }
+
+            public void GetDetails(string Nome, string Cognome, int Eta)
+            {
+                Console.WriteLine("Nome: " + Nome);
+                Console.WriteLine("Cognome: " + Cognome);
+                Console.WriteLine("Età: " + Eta);
+            }
+        }
+    }
---
+++ EsercizioGiorno2S1/Program.cs
@@ -0,0 +1,16 @@
+﻿using EsercizioGiorno2S1.Models;
+
+Persona persona1 = new Persona()
+{
+    Nome = "Giulia",
+    Cognome = "Arbia",
+    Eta = 32,
+};
+
+persona1.GetNome(persona1.Nome);
+Console.WriteLine("-----------");
+persona1.GetCognome(persona1.Cognome);
+Console.WriteLine("-----------");
+persona1.GetEta(persona1.Eta);
+Console.WriteLine("-----------");
+persona1.GetDetails(persona1.Nome, persona1.Cognome, persona1.Eta);
